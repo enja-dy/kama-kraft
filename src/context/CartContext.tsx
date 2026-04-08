86f063599461cc196f7ab954e3b522793ca21cde
Daichi Yokoyama <daichi2@yokoyamadaichinoiMac.local>
2026-04-08 16:56:39 +0900

    step: defined CartItem types and CartContext

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+export interface CartItem {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  image: string;
+}
+
+interface CartContextType {
+  cart: CartItem[];
+  addToCart: (item: CartItem) => void;
+  totalItems: number;
+}
+
+export const CartContext = createContext<CartContextType | undefined>(undefined);
